b2a92c6d97002712a8667700b59100c4af104856
yuanjinyong <yjyffc@163.com>
2017-06-06 14:45:38 +0800

    代码生成规则配置界面。

--- subprojects/backend/src/main/java/com/jeeweb/platform/schema/service/InformationSchemaService.java
+++ subprojects/backend/src/main/java/com/jeeweb/platform/schema/service/InformationSchemaService.java
@@ -15,25 +15,26 @@ import com.jeeweb.framework.core.model.ParameterMap;
 // @Transactional //这个Service只读取数据库元数据，不做任何修改的操作
 public class InformationSchemaService {
     @Autowired
-    @Qualifier("informationSchemaMapper")
+    // @Qualifier("informationSchemaMapper")
+    @Qualifier("jeewebMapper")
     private SqlMapper sqlMapper;
 
     public List<Map<String, Object>> selectSchemataListPage(ParameterMap params) {
-        SqlBuilder sql = new SqlBuilder(params, "SELECT * FROM SCHEMATA WHERE 1=1 \n");
+        SqlBuilder sql = new SqlBuilder(params, "SELECT * FROM information_schema.SCHEMATA WHERE 1=1 \n");
         sql.and("SCHEMA_NAME_like").and("SCHEMA_NAME_notIn");
 
         return sqlMapper.selectListPage(sql.toString(), params);
     }
 
     public List<Map<String, Object>> selectTableListPage(ParameterMap params) {
-        SqlBuilder sql = new SqlBuilder(params, "SELECT * FROM TABLES WHERE 1=1 \n");
+        SqlBuilder sql = new SqlBuilder(params, "SELECT * FROM information_schema.TABLES WHERE 1=1 \n");
         sql.and("TABLE_SCHEMA").and("TABLE_TYPE").and("TABLE_NAME_like").and("TABLE_COMMENT_like");
 
         return sqlMapper.selectListPage(sql.toString(), params);
     }
 
     public List<Map<String, Object>> selectColumnListPage(ParameterMap params) {
-        SqlBuilder sql = new SqlBuilder(params, "SELECT * FROM COLUMNS WHERE 1=1 \n");
+        SqlBuilder sql = new SqlBuilder(params, "SELECT * FROM information_schema.COLUMNS WHERE 1=1 \n");
         sql.and("TABLE_SCHEMA").and("TABLE_NAME").and("TABLE_NAME_like").and("COLUMN_NAME_like")
                 .and("TABLE_COMMENT_like");
 
