d8c0d4dc377910cb37e2d08d35150ac4ac5f52aa
袁进勇 <yjyffc@163.com>
2017-10-01 17:39:30 +0800

    升级webpack、eslint等

--- subprojects/frontend/.eslintrc.js
+++ subprojects/frontend/.eslintrc.js
@@ -17,12 +17,18 @@ module.exports = {
   ],
   //这里写自定义规则
   'rules': {
+    'import/first': 0,
+    'standard/no-callback-literal': [0, ["cb", "callback"]],
+    'no-multiple-empty-lines': 0, // 允许存在多个空行
+    'camelcase': [0, {'properties': 'never'}], //不强制驼峰命名规则
+    'no-multi-spaces': [2, {'ignoreEOLComments': true}], // 注释前允许多个空格
+    'no-useless-return': 1, // 多余的return语句给出警告
     // allow paren-less arrow functions
     'arrow-parens': 0,
-    'camelcase': [0, {'properties': 'never'}], //不强制驼峰命名规则
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 1,
+    'no-alert': process.env.NODE_ENV === 'production' ? 2 : 1
   }
 }
